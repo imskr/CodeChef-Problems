fd5426d084e2d6e92a50450a85e395272ce7880e
Shubham Kumar <shubhamkrai123@gmail.com>
2019-03-04 21:58:45 +0530

    Create Sum of two numbers.cpp

---
+++ Basic Problems/Sum of two numbers.cpp
@@ -0,0 +1,31 @@
+// CHITEST1 - Sum of two numbers
+// Link: https://www.spoj.com/problems/CHITEST1/
+// ID: 23919
+
+// Added by:	chi
+// Date:	2015-05-06
+// Time limit:	1s
+// Source limit:	50000B
+// Memory limit:	1536MB
+// Cluster:	Cube (Intel G860)
+// Languages:	All except: GOSU
+
+/* Solved by: imskr (https://www.spoj.com/users/imskr/)
+   TIME: 0.00
+   MEM:  16M
+   LANG: C++14  */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+  cin>>N;
+  while(N--){
+   	double a, b;
+   	cin>>a>>b;
+   	cout<<a+b<<endl;
+  }
+  return 0;
+}
